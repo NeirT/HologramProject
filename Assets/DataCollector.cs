6085da8d2b6c9cab0f11b626c0245d53e4cb41c1
Acesher <jack.phuc.enderking@gmail.com>
2021-09-23 13:08:00 +0700

    reminder-er

---
+++ Assets/DataCollector.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManager;
+
+public class DataCollector : MonoBehaviour
+{
+    public static GameObject reminder, day, month, year, hour, minute, sec;
+
+    void OnClicked() {
+        SceneManager.LoadSence("4facehologram");
+    }
+}
